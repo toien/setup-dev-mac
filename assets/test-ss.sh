8d678c05883a7ac348a94ca221d85bacac7c3e14
toien <>
2023-12-14 22:13:45 +0800

    Update test-ss.sh

--- assets/test-ss.sh
+++ assets/test-ss.sh
@@ -1,19 +1,16 @@
 
 
-lines=$(curl -s -m 5 $SUBSCRIBE_URL | base64 -D);
+
+lines=$(curl -s -m 5 $SUBSCRIBE_URL | base64 -D)
 # echo $lines
-
-port=$(echo $lines | grep -Eo '.com:[0-9]+' | awk -F ':' '{print $2}' | head -n 1);
-
 
 IFS='
 '
 for line in $lines
 do
-  d=$(echo $line | grep -oE "@(.*):$port" | cut -c 2-);
+  port=$(echo $line | cut -f 1 -d '#' | ggrep -oP ':\K\d+')
+  d=$(echo $line | grep -oE "@(.*):$port" | cut -c 2-)
 
-  resp=$(curl -sI --connect-timeout 3 -m 5 -w "http_code:%{http_code} total_time:%{time_total}" -o /dev/null "https://"$d | sed 's/^.//');
-
-  echo "Testing $d: $resp";
-
+  resp=$(curl -sI --connect-timeout 3 -m 5 -w "http_code:%{http_code} total_time:%{time_total}" -o /dev/null "https://"$d | sed 's/^.//')
+  echo "Testing $d: $resp"
 done
