acf09d29de1a0d0f63cd4aafb6c22ecb937de942
codingXL <>
2024-11-27 21:03:51 +0800

    Fix remove gnu grep

--- assets/test-ss.sh
+++ assets/test-ss.sh
@@ -10,19 +10,19 @@ COLOR_RED='\033[0;31m'
 COLOR_NO='\033[0m'
 
 
-lines=($(curl -s -m 5 $SUBSCRIBE_URL | base64 -D))
+lines=($(curl -s -m 10 $SUBSCRIBE_URL | base64 -D))
 
 for line in "${lines[@]}"
 do
   # echo $line 
-  port=$(echo $line | cut -f 1 -d '#' | ggrep -oP ':\K\d+')
+  port=$(echo $line | cut -f 1 -d '#' | grep -oE ':(\d+)' | cut -c 2-)
   origin=$(echo $line | grep -oE "@(.*):$port" | cut -c 2-)
 
   resp=$(curl -sI --connect-timeout 3 -m 5 -w "http_code:%{http_code} total_time:%{time_total}" -o /dev/null "https://"$origin | sed 's/^.//')
   # echo $resp
   
-  status=$(echo $resp | ggrep -oP 'http_code:\K(\d+)')
-  total_time=$(echo $resp | ggrep -oP 'total_time:\K(\d+(\.\d+)?)')
+  status=$(echo $resp | grep -oE 'http_code:(\d+)' | sed 's/http_code://')
+  total_time=$(echo $resp | grep -oE 'total_time:(\d+(\.\d+)?)' | sed 's/total_time://')
   # echo "$status, $total_time"
 
   if [[ $status -eq 200 && $(echo "$total_time < 3" | bc -l) -eq 1 ]]; then
